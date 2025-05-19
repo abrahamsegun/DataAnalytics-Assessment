d0d8e0e5d9e8cad8387d76fb3f58e0aaf6791b47
OLOWU ABRAHAM <missionaryabra@gmail.com>
2025-05-19 14:50:36 -0700

    Update Assessment_Q4.sql

--- Assessment_Q4.sql
+++ Assessment_Q4.sql
@@ -5,31 +5,35 @@ USE `adashi_staging`;
 
 -- Acct Tenure 
 -- duration a customer have had a acct 
-CREATE OR REPLACE VIEW tenure as (
-    select 
+
+WITH tenure AS (
+    SELECT 
         id AS customer_id,
         CONCAT(first_name, ' ', last_name) AS name,
-        timestampdiff(month, created_on, curdate()) AS tenure_months
-    from users_customuser
-);
+        TIMESTAMPDIFF(MONTH, created_on, CURDATE()) AS tenure_months
+    FROM users_customuser
+),
+-- total transaction per owner-id or customer
 
-CREATE OR REPLACE VIEW total_txn AS (
-    select 
+total_txn AS (
+    SELECT 
         owner_id,
         ROUND(SUM(confirmed_amount) / 100, 2) AS total_transactions
-     from savings_savingsaccount
-     where confirmed_amount > 0
-     group by owner_id
-);
+    FROM savings_savingsaccount
+    WHERE confirmed_amount > 0
+    GROUP BY owner_id
+),
 
-CREATE OR REPLACE VIEW avg_profit AS (
-    select 
-    owner_id,
-    ROUND(AVG(confirmed_amount * 0.001 / 100), 2) AS avg_profit_per_transaction
-	from savings_savingsaccount
-    where confirmed_amount > 0
-    group by owner_id
-);
+-- applying the formula for avgerage profit and turning it to kobo 
+	
+avg_profit AS (
+    SELECT 
+        owner_id,
+        ROUND(AVG(confirmed_amount * 0.001 / 100), 2) AS avg_profit_per_transaction
+    FROM savings_savingsaccount
+    WHERE confirmed_amount > 0
+    GROUP BY owner_id
+)
 
 -- SELECT * FROM tenure;
 -- SELECT * FROM total_txn;
@@ -40,21 +44,14 @@ SELECT
     tn.name,
     tn.tenure_months,
     ttxn.total_transactions,
-    round(((ttxn.total_transactions / NULLIF(tn.tenure_months, 0)) * 12 * ap.avg_profit_per_transaction), 2)
+    ROUND(((ttxn.total_transactions / NULLIF(tn.tenure_months, 0)) * 12 * ap.avg_profit_per_transaction), 2)
     AS estimated_clv
 FROM tenure tn
 JOIN total_txn ttxn
-ON tn.customer_id = ttxn.owner_id
+    ON tn.customer_id = ttxn.owner_id
 JOIN avg_profit ap
-ON tn.customer_id = ap.owner_id
+    ON tn.customer_id = ap.owner_id
 ORDER BY estimated_clv DESC;
 
--- using the provided formula for avg_profit_per_transaction 
 -- replacing null months with zeros for mor accuracy     
 -- round(((ttxn.total_transactions / NULLIF(tn.tenure_months, 0)) * 12 * ap.avg_profit_per_transaction), 2)
-
-
-
-
-
-
